61d95d041d2f7f8f34f1b0b7419425cf946ab90c
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2017-04-06 17:04:00 +0000

    change main to get args

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sys
 from flare_prediction import run
 
 if __name__ == "__main__":
-	#run(sys.argv[1:])
+	run(sys.argv[1:])
 
 	#run(["local", "min_max", "norm_min_max"])
-	run(["local", "z_score", "norm_z_score"])
+	#run(["local", "z_score", "norm_z_score"])
 	#run(["local", "zero_center", "norm_zero_center"])
 	#run(["local", "pca_whiten", "norm_pca_whiten"])
